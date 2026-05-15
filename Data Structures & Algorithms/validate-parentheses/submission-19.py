69e8cc52566a6341b135a5ff73fb32d4f695a7c9
Ayush Pawar <ayushmpawar@gmail.com>
2026-05-15 09:30:30 -0400

    Add: validate-parentheses - submission-19

---
+++ Data Structures & Algorithms/validate-parentheses/submission-19.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # T: O(n), S: O(n)
+        cto = { ")" : "(", "}" : "{", "]" : "["}
+        stack = []
+
+        for c in s:
+            if c not in cto:
+                stack.append(c)
+                continue
+            
+            if not stack or stack[-1] != cto[c]:
+                return False
+
+            stack.pop()
+
+        return not stack
